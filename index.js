1d1bfa18c12f623b366b34f8c5c7da1c33f98ca0
vitor hugo <vitorhugoif@gmail.com>
2019-09-24 23:10:55 -0300

    criaçao de um servidor com node e uso da template engine EJS

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const expressLayouts = require('express-ejs-layouts');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', (req, res) => {
+    res.render('pages/home');
+});
+
+
+app.listen(port, () => {
+    console.log(`Run in  http://localhost:${port}`);
+})
